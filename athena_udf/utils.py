6be9aa880d1b47631f5e16368ecc86c3f4805120
Serhii Dimchenko <svdimchenko@gmail.com>
2024-03-28 12:14:15 +0100

    Fixed library files structure

--- athena_udf/utils.py
+++ athena_udf/utils.py
@@ -7,4 +7,4 @@ T = TypeVar("T")
 def get_chunks(lst: list[T], n: int) -> Generator[list[T], None, None]:
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
-        yield lst[i : i + n]
+        yield lst[i: i + n]
