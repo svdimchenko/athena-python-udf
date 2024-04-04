e09e075580cad0a724fe9dc7cdc0191c68a73a61
Serhii Dimchenko <svdimchenko@gmail.com>
2024-04-04 08:39:53 +0200

    Added multithreading tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,11 @@
 import pytest
 
-from athena_udf.utils import get_chunks, process_records
+from athena_udf.utils import (
+    Record,
+    get_chunks,
+    process_records,
+    process_records_in_chunks,
+)
 
 
 def test_get_chunks_empty():
@@ -22,26 +27,72 @@ def test_get_chunks_more_elements_than_chunk():
 
 # Test data
 @pytest.fixture
-def mock_records():
+def mock_records_int():
     return [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}]
 
 
-# Test cases using pytest.parametrize
+@pytest.fixture
+def mock_records_str():
+    return [{"a": "a", "b": "b"}, {"a": "c", "b": "d"}, {"a": "e", "b": "f"}]
+
+
+@pytest.mark.parametrize(("args", "expected_output"), [((1, 2), [6, 10, 14]), ((3, 4), [10, 14, 18])])
+def test_process_records_sum(args: tuple, mock_records_int: list[Record], expected_output: list[int]):
+    def mock_func(arg1: int, arg2: int, records: list[Record]) -> int:
+        return sum(records) + arg1 + arg2
+
+    actual_output = process_records(mock_func, args, mock_records_int)
+    assert sorted(actual_output) == sorted(expected_output)
+
+
 @pytest.mark.parametrize(
-    "func, args, mock_records, expected_output",
-    [
-        pytest.param(lambda *args: sum(mock_records), (1, 2), [3, 7, 11]),
-        pytest.param(lambda *args: sum(mock_records()), (3, 4), [7, 11, 15]),
-        pytest.param(lambda *args, records: "".join(args), (), ["helloworld", "foobar"]),
-    ],
-    indirect=["mock_records"],
+    ("args", "expected_output"), [(("x", "y"), ["abxy", "cdxy", "efxy"]), (("z", "w"), ["abzw", "cdzw", "efzw"])]
 )
-def test_process_records(func, args, expected_output, mock_records):
-    actual_output = process_records(func, args, mock_records)
-    assert actual_output == expected_output
+def test_process_records_concat(args: tuple, mock_records_str: list[Record], expected_output: list[str]):
+    def mock_func(arg1, arg2, records: list[Record]) -> str:
+        return "".join(records) + arg1 + arg2
+
+    actual_output = process_records(mock_func, args, mock_records_str)
+    assert sorted(actual_output) == sorted(expected_output)
 
 
-# Additional test case for empty records
-def test_process_records_empty(mock_records):
-    actual_output = process_records(lambda *args: sum(args), (1, 2), [])
-    assert actual_output == []
+@pytest.mark.parametrize(
+    ("args", "chunk_size", "expected_output"),
+    [
+        ((1, 2), 1, [6, 10, 14]),
+        ((1, 2), 2, [6, 10, 14]),
+        ((1, 2), 3, [6, 10, 14]),
+        ((3, 4), 1, [10, 14, 18]),
+        ((3, 4), 2, [10, 14, 18]),
+        ((3, 4), 3, [10, 14, 18]),
+    ],
+)
+def test_process_records_in_chunks_sum(
+    args: tuple, mock_records_int: list[Record], chunk_size: int, expected_output: list[int]
+):
+    def mock_func(arg1: int, arg2: int, records: list[Record]) -> int:
+        return sum(records) + arg1 + arg2
+
+    actual_output = process_records_in_chunks(mock_func, args, mock_records_int, chunk_size)
+    assert sorted(actual_output) == sorted(expected_output)
+
+
+@pytest.mark.parametrize(
+    ("args", "chunk_size", "expected_output"),
+    [
+        (("x", "y"), 1, ["abxy", "cdxy", "efxy"]),
+        (("x", "y"), 2, ["abxy", "cdxy", "efxy"]),
+        (("x", "y"), 3, ["abxy", "cdxy", "efxy"]),
+        (("z", "w"), 1, ["abzw", "cdzw", "efzw"]),
+        (("z", "w"), 2, ["abzw", "cdzw", "efzw"]),
+        (("z", "w"), 3, ["abzw", "cdzw", "efzw"]),
+    ],
+)
+def test_process_records_in_chunks_concat(
+    args: tuple, mock_records_str: list[Record], chunk_size: int, expected_output: list[str]
+):
+    def mock_func(arg1, arg2, records: list[Record]) -> str:
+        return "".join(records) + arg1 + arg2
+
+    actual_output = process_records_in_chunks(mock_func, args, mock_records_str, chunk_size)
+    assert sorted(actual_output) == sorted(expected_output)
